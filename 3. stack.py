94b89071ad1b38e756eb98e1ae12a3bfc9e45463
Seng Keat <corodostudio@gmail.com>
2022-09-29 16:16:34 +0700

    Update 3. stack.py

--- 3. stack.py
+++ 3. stack.py
@@ -1,1 +1,10 @@
+# in creating a stack we borrow both of the form of the array [] and its members
+# Here are their operations
+# The functions associated with stack are:
+# empty() – Returns whether the stack is empty – Time Complexity: O(1)
+# size() – Returns the size of the stack – Time Complexity: O(1)
+# top() / peek() – Returns a reference to the topmost element of the stack – Time Complexity: O(1)
+# push(a) – Inserts the element ‘a’ at the top of the stack – Time Complexity: O(1)
+# pop() – Deletes the topmost element of the stack – Time Complexity: O(1)
+
 
