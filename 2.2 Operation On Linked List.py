bb9b03b343fe07da18da1ae1e83884d0ee961831
Seng Keat <corodostudio@gmail.com>
2022-09-29 15:05:39 +0700

    Create 2.2 Operation On Linked List.py

---
+++ 2.2 Operation On Linked List.py
@@ -0,0 +1,9 @@
+# Here are the operation upon the linked list
+    # Traversal: To traverse all the nodes one after another.
+    # Insertion: To add a node at the given position.
+    # Deletion: To delete a node.
+    # Searching: To search an element(s) by value.
+    # Updating: To update a node.
+    # Sorting: To arrange nodes in a linked list in a specific order.
+    # Merging: To merge two linked lists into one.
+    # Copy : Copy operations are used to create a duplicate of a data structure. This can be done by copying each element in the original data structure to the new one.
