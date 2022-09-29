8fac2efe3bcc0e02348e1db20131563f2fd04254
Seng Keat <corodostudio@gmail.com>
2022-09-29 15:21:43 +0700

    Update 1. array.py

--- 1. array.py
+++ 1. array.py
@@ -1,1 +1,5 @@
+# In computer science, an array is a data structure consisting of a collection of elements, each identified by at least one array index or key. An array is stored such that the position of each element can be computed from its index tuple by a mathematical formula.
+# In creating the array in any programming language the different is just the syntax
+
+# when you do need to use the array? In programming, most of the cases need to store a large amount of data of a similar type. We need to define numerous variables to store such a huge amount of data. While writing the programs, it would be very tough to memorize all variable names. Instead, it is better to define an array and store all the elements in it. The following example illustrates how an array can be used while writing a code- In the following example, we have given marks to a student in 5 different subjects. The aim is to calculate the average of a student’s marks.
 
